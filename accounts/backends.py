ed883bbd84cf8d6f7ca7b356a69f2d5093bbd186
Suttonium <raymond.sutton.15@cnu.edu>
2019-01-21 11:16:40 -0500

    Created the custom backends for both email and student ID login authentication. Added these new backends to the settings.py file so Django can use them for authentication.

---
+++ accounts/backends.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+
+
+class EmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        user_model = get_user_model()
+        try:
+            user = user_model.objects.get(email=username)
+        except user_model.DoesNotExist:
+            return None
+        else:
+            if user.check_password(password):
+                return user
+        return None
+
+
+class StudentIDBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        user_model = get_user_model()
+        try:
+            user = user_model.objects.get(student_id=username)
+        except user_model.DoesNotExist:
+            return None
+        else:
+            if user.check_password(password):
+                return user
+        return None
