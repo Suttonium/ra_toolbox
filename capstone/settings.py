783effc30bf0e8b76b6c6646475fb13266d535d5
Suttonium <raymond.sutton.15@cnu.edu>
2019-01-24 10:29:28 -0500

    test...

--- capstone/settings.py
+++ capstone/settings.py
@@ -129,6 +129,8 @@ AUTHENTICATION_BACKENDS = ('accounts.backends.EmailBackend', 'accounts.backends.
 
 EMAIL_USE_TLS = True
 # EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "residentassistanttoolbox@gmail.com"
+EMAIL_HOST_PASSWORD = "Havoc1996"
 EMAIL_HOST = socket.gethostbyname('smtp.gmail.com')
 EMAIL_PORT = 587
 
