3b57fb3e7bf7eddb6a5881ea57683e49cd386154
Suttonium <raymond.sutton.15@cnu.edu>
2019-01-24 10:11:47 -0500

    Added views to handle the new signup forms. Templates do not exist yet. Updated settings.py to account for Sites application getting the current domain. Will have to write a script to populate DB with residence halls and update the sites app to reference the site url. Updated urls.py.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,27 @@
-from django.views.generic import TemplateView
-
+from django.urls import reverse
+from django.views.generic import TemplateView, CreateView
 
 # Create your views here.
+from accounts.forms import *
+
+
 class SignupTypeDecisionView(TemplateView):
     template_name = 'accounts/ra_or_student.html'
+
+
+class StudentSignUpView(CreateView):
+    form_class = StudentRegistrationForm
+    model = User
+    template_name = 'accounts/studentsignup_form.html'
+
+    def get_success_url(self):
+        return reverse('accounts:login')
+
+
+class ResidentAssistantSignUpView(CreateView):
+    form_class = ResidentAssistantRegistrationForm
+    model = User
+    template_name = 'accounts/residentassistantsignup_form.html'
+
+    def get_success_url(self):
+        return reverse('accounts:login')
