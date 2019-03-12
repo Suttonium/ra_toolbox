c01f0515a19d63569beb9796a76d2b885d468082
Raymond Sutton <raymond.sutton.15@cnu.edu>
2019-03-12 17:16:50 -0400

    i hate frontend

--- desklogs/views.py
+++ desklogs/views.py
@@ -9,7 +9,7 @@ from desklogs.models import GuestLogEntry
 
 class GuestLogEntryListView(LoginRequiredMixin, ListView):
     model = GuestLogEntry
-    template_name = 'desklogs/desklog.html'
+    template_name = 'desklogs/guestlog.html'
 
     def get_context_data(self, *args, object_list=None, **kwargs):
         context = super().get_context_data(*args, **kwargs)
